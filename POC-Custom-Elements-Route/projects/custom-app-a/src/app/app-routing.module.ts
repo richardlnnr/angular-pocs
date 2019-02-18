a3b1b61d1c30c2e0b5ff1288ef45e0d6f775d47f
Richard Lindner <richard.lnnr@gmail.com>
2019-02-18 10:58:42 -0300

    Add people list to router

--- POC-Custom-Elements-Route/projects/custom-app-a/src/app/app-routing.module.ts
+++ POC-Custom-Elements-Route/projects/custom-app-a/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PeopleListComponent } from './people-list/people-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'list', component: PeopleListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
